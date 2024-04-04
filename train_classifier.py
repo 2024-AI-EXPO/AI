78adaf02adad2ec6a550e6a4603ad3b84b86d177
Sepro <shtmdwns0@gmail.com>
2024-04-04 21:51:59 +0900

    dataset

--- train_classifier.py
+++ train_classifier.py
@@ -5,23 +5,24 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
 
-data_dict = pickle.load(open('data link', 'rb'))
+data_dict = pickle.load(open('data.pickle', 'rb'))
 
-data = np.asarray(data_dict['data'])
-labels = np.asarray(data_dict['labels'])
+labels = np.array(data_dict['labels'])
+for d in data_dict['data']:
+    print(*d)
+data = np.array(data_dict['data'], dtype=np.float32)
 
-x_train, x_test, y_train, y_test = train_test_split(data, labels ,test_size=0.2 ,shuffle=True, stratify=labels)
+x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
 model = RandomForestClassifier()
 
-model.fit(x_train,y_train)
+model.fit(x_train, y_train)
 
 y_predict = model.predict(x_test)
 
 score = accuracy_score(y_predict, y_test)
 
-print('{}% 학습 되었습니다.'.format(score * 100))
+print(f'{score * 100}% 학습 되었습니다.')
 
-f = open('model.p', 'wb')
-pickle.dump({'model':model}, f)
-f.close()
+with open('model.p', 'wb') as f:
+    pickle.dump({'model': model}, f)
